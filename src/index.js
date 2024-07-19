f2c93ae759ee5b5ff3b273839e0348c08512fb5d
PHOSDA <panhaphosda@gmail.com>
2024-07-19 16:17:47 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,10 +11,27 @@ function App() {
     // const time = date.toLocaleTimeString();
 
     // return <h1>{time}</h1>;
+    // return <h1>{new Date().toLocaleTimeString()}</h1>;
+
+    /* const name = 'dada';
+    const age = 20;
+
+        return (
+        <h1>
+            Hi , my name is {name} and my age is {age}
+        </h1>
+    ); */
 
 
-    return <h1>{new Date().toLocaleTimeString()}</h1>;
+    /*const inputType = 'number';
+    const minValue = 5;
 
+    return <input type = {inputType} min={minValue}/>;*/
+    // return <input style={{border:'3px solid red'}}type="number" min={5} />;
+
+    // return < textarea autofocus ={false} />;
+
+    return <input maxLength={5} />;
 }
 // 5, show the component on the screen 
 root.render(<App />);
